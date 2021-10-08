5eddd3604e86d8d26baec6a9d4ce09450b7b9d40
Stanislav <stas.sor@gmail.com>
2021-10-08 11:46:03 +0300

    Завершено ознакомление со стрелочной функцией

--- 2_arrow_functions.js
+++ 2_arrow_functions.js
@@ -22,14 +22,33 @@
 function log() {
     console.log(this)
 }
-
 const arrowLog = () => console.log(this)
+
+// const person = {
+//     name: 'elena',
+//     age: 20,
+//     log: log,
+//     arrowLog: arrowLog,
+//     delayLog: function() {
+//         const self = this
+//         global.setTimeout(function() {
+//             console.log(self.name + ' ' + self.age)
+//         }, 1000)
+//     }
+// }
+//
+// person.delayLog()
 
 const person = {
     name: 'elena',
     age: 20,
     log: log,
-    arrowLog: arrowLog
+    arrowLog: arrowLog,
+    delayLog: function() {
+        global.setTimeout(() => {
+            console.log(this.name + ' ' + this.age)
+        }, 1000)
+    }
 }
 
-person.log()
+person.delayLog()
