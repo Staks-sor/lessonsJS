ee89d6052bb134e2136076d930ac7c8b23370adb
Stanislav <stas.sor@gmail.com>
2021-10-08 10:37:34 +0300

    context

--- 2_arrow_functions.js
+++ 2_arrow_functions.js
@@ -6,8 +6,30 @@
 //     return a ** 3
 // }
 
-const sum = (a, b) => a + b
-const cube = a => a ** 3
+// const sum = (a, b) => a + b
+// const cube = a => a ** 3
 
-console.log(sum(41, 1))
-console.log(cube(2))
+// console.log(sum(41, 1))
+// console.log(cube(2))
+
+// setTimeout(function() {
+//     console.log('After 1 seconds')
+// }, 1000)
+
+// setTimeout(() => console.log('After 1 seconds'), 1000)
+
+// Context
+function log() {
+    console.log(this)
+}
+
+const arrowLog = () => console.log(this)
+
+const person = {
+    name: 'elena',
+    age: 20,
+    log: log,
+    arrowLog: arrowLog
+}
+
+person.log()
