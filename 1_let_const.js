08ac60dbfe2df9e5f0a14a3bed6b6c6c185ce17b
Stanislav <stas.sor@gmail.com>
2021-10-08 09:41:19 +0300

    отличия от константы и let и var.

--- 1_let_const.js
+++ 1_let_const.js
@@ -1,10 +1,40 @@
-let a = 234
+// let a = 234
+//
+// if (true) {
+//
+//     let a = 46
+//
+//     console.log('if', a)
+// }
+//
+// console.log(a)
 
-if (true) {
+// Hoisting
+// b = 20
+// console.log(b)
+// let b = 10
 
-    let a = 46
+// function hoisting() {
+//     age = 26
+// }
+//
+// let age
+// hoisting()
+// console.log(age)
 
-    console.log('if', a)
-}
+// Const
+const COLOR = '#ffeebb'
+// COLOR = '#000'
 
-console.log(a)
+// const array = [1, 2, 3, 5, 8]
+//
+// console.log(array)
+//
+// array.push(13)
+//
+// console.log(array)
+
+const obj = {a: 42}
+obj.name = 'Igor'
+
+console.log(obj)
