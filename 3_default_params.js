b080f3481e3876615a71c5304580612ab6b589e2
Stanislav <stas.sor@gmail.com>
2021-10-08 12:04:30 +0300

    дуфолтное значение

---
+++ 3_default_params.js
@@ -0,0 +1,8 @@
+const defaultB = 30
+const getDefault = c => c * 2
+// дефолтные значения
+function compute(a = 10, b = getDefault(a)) {
+    return a + b
+}
+// можно задать числовые значения другие
+console.log(compute())
