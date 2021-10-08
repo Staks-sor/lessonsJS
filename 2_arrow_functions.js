1e32ddaa5b972d6c1acf3f51e4dc50c3d24a104e
Stanislav <stas.sor@gmail.com>
2021-10-08 09:58:20 +0300

    стрелочная функция js

---
+++ 2_arrow_functions.js
@@ -0,0 +1,13 @@
+// function sum(a, b) {
+//     return a + b
+// }
+//
+// function cube (a) {
+//     return a ** 3
+// }
+
+const sum = (a, b) => a + b
+const cube = a => a ** 3
+
+console.log(sum(41, 1))
+console.log(cube(2))
