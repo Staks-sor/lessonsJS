f6d4eac4bad12214b0ae76e1b71febe7d2452942
Stanislav <stas.sor@gmail.com>
2021-10-12 08:16:11 +0300

    обратные ковычки

--- 4_strings.js
+++ 4_strings.js
@@ -1,12 +1,12 @@
 // Новая фукция обратные ковычки `` в которых можно передать значени используя и обычные ковычки
 // запоменает проблемы.
-const title = 'hello'
-
-const isVisible = () => Math. random() > 0.5
-
-const template = `
-    ${isVisible() ? `<p>Visible</p>` : 'huy'}
-    <h1 id='demo' style="color: red">${title}</h1>
-`
-
-console.log(template)
+// const title = 'hello'
+//
+// const isVisible = () => Math. random() > 0.5
+//
+// const template = `
+//     ${isVisible() ? `<p>Visible</p>` : 'random'}
+//     <h1 id='demo' style="color: red">${title}</h1>
+// `
+//
+// console.log(template)
